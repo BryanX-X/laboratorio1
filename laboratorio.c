207a926136a61eba6a0b732fb4aa543f784ff428
BryanX-X <bryan.adrian14042006@gmail.com>
2026-04-09 10:10:22 -0500

    version 1

---
+++ laboratorio.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+int main(){
+    float num1;
+    float num2;
+
+    while (num1 <= 0 || num2 <= 0){
+    printf("Ingrese la nota 1: \n"); scanf("%f", &num1);
+    printf("Ingrese la nota 2: \n"); scanf("%f", &num2);
+
+
+    if (num1 >= 0 && num2 >= 0){
+        float prom = (num1 + num2) / 2;
+        printf("Su promedio es %.2f\n", prom);
+
+    if(prom >= 14){
+        printf("Aprobado\n");
+    }else{
+        printf("Reprobado\n");
+    }
+        }else{
+        printf("No se puede ingresar numeros negativos\n");
+
+        }
+    }
+}
